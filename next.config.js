82a3153f2e3cb203a0e151b344404651cf2956a0
rudijetson <info@engineer.marketing>
2024-12-11 13:00:37 -0500

    Fix: Update dependencies and configuration

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ['react-simple-maps'],
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = nextConfig 
